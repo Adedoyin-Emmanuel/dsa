aabe0e2d14c86a0006a73590639dbcff9247e662
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-02-29 19:34:50 +0100

    Feat: Worked on pop and push

---
+++ practice/array-methods/array-method.ts
@@ -0,0 +1,16 @@
+// Let's start with popping and pushing
+
+const cars = ["Lamboghini", "Bugatti", "Henenssey", "Volvo"];
+
+const newCar = cars.push("Maruwa");
+
+console.log(`New car is ${newCar}`);
+console.log(cars);
+
+const fruits = ["Banana", "Mango", "Strawberry", "Guava", "Berry"];
+
+const poppedFruit = fruits.pop();
+
+console.log(`Popped fruit is ${poppedFruit}`);
+
+console.log(fruits);
