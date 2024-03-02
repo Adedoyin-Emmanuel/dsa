ca31b062aa04d3b49380c65777b3d1a49bba42e4
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-03-02 22:48:43 +0100

    Feat: Finished set intersection

--- algo/set.ts
+++ algo/set.ts
@@ -15,6 +15,10 @@ class MySet {
     return this.collection.indexOf(element) !== -1;
   };
 
+  public values = () => {
+    return this.collection;
+  };
+
   public add = (element) => {
     // check for duplicate
     if (this.has(element)) return false;
@@ -31,6 +35,35 @@ class MySet {
 
     return true;
   };
+
+  public intersection = (set) => {
+    const firstSet = this.collection;
+    const intersectionSet = new MySet();
+
+    firstSet.forEach((element, index) => {
+      if (set.has(element)) {
+        intersectionSet.add(element);
+      }
+    });
+
+    return intersectionSet;
+  };
+
+  public union = (set) => {
+    const firstSet = this.values();
+    const secondSet = set.values();
+    const unionSet = new MySet();
+
+    firstSet.forEach((element, index) => {
+      unionSet.add(element);
+    });
+
+    secondSet.forEach((element, index) => {
+      unionSet.add(element);
+    });
+
+    return unionSet;
+  };
 }
 
 const set1 = new MySet();
@@ -38,6 +71,17 @@ const set1 = new MySet();
 set1.add("ant");
 set1.add("bata");
 
-console.log(set1.remove("ant"));
+const set2 = new MySet();
 
-console.log(set1.length());
+set2.add("baba");
+set2.add("m4a1");
+
+set2.add("ant");
+set2.add("bata");
+set1.add("baba");
+set1.add("m4a1");
+
+const result = set1.union(set2);
+
+console.log(result.length());
+console.log(result.values());
