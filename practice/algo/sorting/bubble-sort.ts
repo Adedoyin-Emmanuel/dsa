b65b9de97b686c06d95df416a0ae4492c393fed2
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-01-12 19:28:22 +0100

    tests: bubbleSortArray test passed

--- practice/algo/sorting/bubble-sort.ts
+++ practice/algo/sorting/bubble-sort.ts
@@ -14,6 +14,9 @@ const bubbleSortArray = (arr: number[]) => {
   return arr;
 };
 
-var result = bubbleSortArray([4, 3, 2, 1]);
+console.log(bubbleSortArray([4, 3, 1, 2])); // [1, 2, 3, 4]
+console.log(bubbleSortArray([5, 1, 4, 2, 8])); // [1, 2, 4, 5, 8]
+console.log(bubbleSortArray([1, 2, 3, 4])); // [1, 2, 3, 4] (already sorted)
+console.log(bubbleSortArray([4, 3, 2, 1])); // [1, 2, 3, 4] (reverse order)
+console.log(bubbleSortArray([5, 5, 3, 1, 2])); // [1, 2, 3, 5, 5] (duplicate values)
 
-console.log(result);
