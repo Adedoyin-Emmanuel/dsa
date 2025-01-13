8b7fbdc939b25a76760dd149a19f01391e0eeb5c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-01-13 03:06:36 +0100

    feat: implemented selection sort

---
+++ practice/algo/sorting/selection-sort.ts
@@ -0,0 +1,27 @@
+const selectionSortArray = (arr: number[]) => {
+  /**
+   *
+   * We have 2 things to do here
+   *
+   * 1. Check the lowest item in the array
+   * 2. Swap the item to the respective index
+   */
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    var lowestItemIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowestItemIndex]) {
+        lowestItemIndex = j;
+      }
+    }
+    var temp = arr[i];
+    arr[i] = arr[lowestItemIndex];
+    arr[lowestItemIndex] = temp;
+  }
+
+  return arr;
+};
+
+var result = selectionSortArray([7, 2, 1, 6, 4]);
+
+console.log(result);
